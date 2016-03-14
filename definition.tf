3a537c3f7ebcbf20f1331375ffa684bbb92d09f6
Paulo Ragonha <paulo@ragonha.me>
2016-03-14 21:56:44 +0100

    Assign a floating IP so that we can recreate machines while maintaining the same public IP 🍀
    For more information: https://cloud.digitalocean.com/support/suggestions?query=Floating+IPs+cost

--- definition.tf
+++ definition.tf
@@ -4,14 +4,19 @@ provider "digitalocean" {
 
 resource "digitalocean_domain" "macaco-maluco" {
   name = "macacomaluco.space"
-  ip_address = "${digitalocean_droplet.macaco-maluco.ipv4_address}"
+  ip_address = "${digitalocean_floating_ip.macaco-maluco.ip_address}"
 }
 
 resource "digitalocean_record" "macaco-maluco" {
   domain = "${digitalocean_domain.macaco-maluco.name}"
   type = "A"
   name = "*"
-  value = "${digitalocean_droplet.macaco-maluco.ipv4_address}"
+  value = "${digitalocean_floating_ip.macaco-maluco.ip_address}"
+}
+
+resource "digitalocean_floating_ip" "macaco-maluco" {
+  droplet_id = "${digitalocean_droplet.macaco-maluco.id}"
+  region = "${digitalocean_droplet.macaco-maluco.region}"
 }
 
 resource "digitalocean_droplet" "macaco-maluco" {
@@ -20,8 +25,10 @@ resource "digitalocean_droplet" "macaco-maluco" {
   region = "nyc2"
   size = "512mb"
   ssh_keys = [15143]
+  ipv6 = true
+  private_networking = true
 }
 
 output "ip" {
-  value = "${digitalocean_droplet.macaco-maluco.ipv4_address}"
+  value = "${digitalocean_floating_ip.macaco-maluco.ip_address}"
 }
